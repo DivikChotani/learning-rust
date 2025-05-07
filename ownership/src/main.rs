7a6c20a99cf1af77b72e3816132e5f13c2c0cc78
Divik Chotani <divik.chotani@gmail.com>
2025-05-06 23:27:30 -0700

    references and borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -35,6 +35,38 @@ fn main() {
     makes_copy(y);
     println!("x still valid: {x}");
 
+    //References and Borrowing, immutable by default
+    let mut s1 = String::from("Hairy");
+
+
+    let len = calculate_len(&s1); // passing by reference
+
+    println!("Len of {s1} is {len}");
+
+    let temp = &mut s1;
+    // After this point, s1 and temp cannot be used at the same time.
+    // However, once temp is no longer used, s1 becomes usable again.
+
+    passing_mut_ref(temp);
+
+    println!("{s1}");
+
+    {
+        let r1 = &mut s; 
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r2 = &mut s;
+    //println!("{temp}");
+
+    let mut hello = String::from("hello");
+
+    let hellor1 = &s; // no problem
+    let hellor2 = &s; // no problem
+    println!("{hellor1} and {hellor2}");
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
+
 }
 
 fn takes_ownership(s : String){
@@ -43,6 +75,16 @@ fn takes_ownership(s : String){
 
 fn makes_copy(t: i32){
     println!("copy of variable t: {t}, not same reference");
-} //Here, some_string goes out of scope and `drop` is called. The backing
+} 
+//Here, some_string goes out of scope and `drop` is called. The backing
 // memory is freed.
 
+
+fn calculate_len(refs : &String) -> usize {
+    return refs.len();
+} //refs does not have ownership of the string, so the value is not dropped
+
+
+fn passing_mut_ref(t: & mut String) {
+    t.push_str("Brandan");
+}
