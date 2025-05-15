dd739f39fe4ec1d398ad64a9ba60a42fc41c38ca
Divik Chotani <divik.chotani@gmail.com>
2025-05-15 14:01:06 -0700

    strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -52,6 +52,56 @@ fn main() {
         cells.push(SpreadsheetCell::Int(2));
     }
 
-    println!("{cells:#?}")
+    println!("{cells:#?}");
+
+    //STRINGS
+
+    let mut s1 = String::from("value");
+    s1.push('1'); //push adds a single char
+
+    s1.push_str("diddy"); //push str takes &str, (strings can also go, deref coersion)
+
+    let q = String::from("HI");
+    s1.push_str(&q);// but must pass reference to string, so q will still have ownership
+
+    println!("{s1} {q}");
+
+    let mut s2 = s1 + &q; // addition takes string and &str, so s1 looses access here
+
+    //s1.clear(); will throw error because s1 not valid anymore
+    s2.clear(); //this is allowed
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3; //works left to right, "-" is a string literal, so it
+    //is already a &str
+
+    println!("{s}");
+
+    //whenever using [], must use &
+    //let mut k = s[0..1]; not allowed
+    let p = &s[0..1];
+    println!("{p}");
+
+    //to get nth char
+    let t = s.chars().nth(2); //nth is not O(1)
+    //if let statement
+    if let Some(ch) = t {
+        println!("t is a valid index, pointing to {ch}"); //use ch, because that is the unwrapped char provided if the if let succeeds
+        // ch = 'q';
+        // println!("{s}"); won't modify the array, nth returns a copy
+    }
+    else{
+        println!("FAILED");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}"); //this will print the byte values, each char is two bytes here
+        //so it will print two bytes
+    }
+
+
 
 }
